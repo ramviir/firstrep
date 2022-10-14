d9922e6051e451e57263613f8a55762b342ca890
Denis Kozhevnikov <ramviir@gmail.com>
2022-10-14 20:18:34 +0300

    Initial commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by user on 14.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
